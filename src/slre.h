987ad51cf77d84fcdd0f222ae3b54210da8b06ae
RyBack <mohabhassan312@gmail.com>
2018-06-18 13:52:45 +0200

    VS2017 Update

--- src/slre.h
+++ src/slre.h
@@ -1,48 +1,48 @@
 /*
- * Copyright (c) 2004-2013 Sergey Lyubka <valenok@gmail.com>
- * Copyright (c) 2013 Cesanta Software Limited
- * All rights reserved
- *
- * This library is dual-licensed: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation. For the terms of this
- * license, see <http://www.gnu.org/licenses/>.
- *
- * You are free to use this library under the terms of the GNU General
- * Public License, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * Alternatively, you can license this library under a commercial
- * license, as set out in <http://cesanta.com/products.html>.
- */
+* Copyright (c) 2004-2013 Sergey Lyubka <valenok@gmail.com>
+* Copyright (c) 2013 Cesanta Software Limited
+* All rights reserved
+*
+* This library is dual-licensed: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 2 as
+* published by the Free Software Foundation. For the terms of this
+* license, see <http://www.gnu.org/licenses/>.
+*
+* You are free to use this library under the terms of the GNU General
+* Public License, but WITHOUT ANY WARRANTY; without even the implied
+* warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU General Public License for more details.
+*
+* Alternatively, you can license this library under a commercial
+* license, as set out in <http://cesanta.com/products.html>.
+*/
 
 /*
- * This is a regular expression library that implements a subset of Perl RE.
- * Please refer to README.md for a detailed reference.
- */
+* This is a regular expression library that implements a subset of Perl RE.
+* Please refer to README.md for a detailed reference.
+*/
 
-#ifndef SLRE_HEADER_DEFINED
-#define SLRE_HEADER_DEFINED
+#ifndef CS_SLRE_SLRE_H_
+#define CS_SLRE_SLRE_H_
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-struct slre_cap {
-  const char *ptr;
-  int len;
-};
+	struct slre_cap {
+		const char *ptr;
+		int len;
+	};
 
 
-int slre_match(const char *regexp, const char *buf, int buf_len,
-               struct slre_cap *caps, int num_caps, int flags);
+	int slre_match(const char *regexp, const char *buf, int buf_len,
+		struct slre_cap *caps, int num_caps, int flags);
 
-/* Possible flags for slre_match() */
-enum { SLRE_IGNORE_CASE = 1 };
+	/* Possible flags for slre_match() */
+	enum { SLRE_IGNORE_CASE = 1 };
 
 
-/* slre_match() failure codes */
+	/* slre_match() failure codes */
 #define SLRE_NO_MATCH               -1
 #define SLRE_UNEXPECTED_QUANTIFIER  -2
 #define SLRE_UNBALANCED_BRACKETS    -3
@@ -57,4 +57,4 @@ enum { SLRE_IGNORE_CASE = 1 };
 }
 #endif
 
-#endif  /* SLRE_HEADER_DEFINED */
+#endif /* CS_SLRE_SLRE_H_ */
