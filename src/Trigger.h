f7fd288902677f084b50575fbd5df3ca72b6637d
mohabhassan <mohabhassan312@gmail.com>
2020-12-16 16:30:04 +0200

    Add Trigger and SafePtr classes.

---
+++ src/Trigger.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "Animate.h"
+
+class ScriptThreadLabel
+{
+    void* m_Script;//GameScript*
+    const_str m_Label;
+};
+
+class Trigger :	public Animate
+{
+#ifndef MOHAA
+#error "Only Mohaa supported for this class."
+#endif // MOHAA
+protected:
+    float wait; /* bitsize 32, bitpos 7200 */
+    float delay; /* bitsize 32, bitpos 7232 */
+    float trigger_time; /* bitsize 32, bitpos 7264 */
+    qboolean triggerActivated; /* bitsize 32, bitpos 7296 */
+    int count; /* bitsize 32, bitpos 7328 */
+    const_str noise; /* bitsize 32, bitpos 7360 */
+    const_str message; /* bitsize 32, bitpos 7392 */
+    ScriptThreadLabel /* id 260 */ m_Thread; /* bitsize 64, bitpos 7424 */
+    SafePtr<Entity> activator; /* bitsize 128, bitpos 7488 */
+    int respondto; /* bitsize 32, bitpos 7616 */
+    qboolean useTriggerDir; /* bitsize 32, bitpos 7648 */
+    float triggerCone; /* bitsize 32, bitpos 7680 */
+    Vector triggerDir; /* bitsize 96, bitpos 7712 */
+    float triggerDirYaw; /* bitsize 32, bitpos 7808 */
+    qboolean triggerable; /* bitsize 32, bitpos 7840 */
+    qboolean removable; /* bitsize 32, bitpos 7872 */
+    qboolean edgeTriggered; /* bitsize 32, bitpos 7904 */
+    int multiFaceted; /* bitsize 32, bitpos 7936 */
+};
+
