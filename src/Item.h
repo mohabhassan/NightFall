5930cc08cf6072e5ee85f7916aed25304d2d7279
mohabhassan <mohabhassan312@gmail.com>
2020-12-16 16:47:49 +0200

    Add Item and Weapon classes.

---
+++ src/Item.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Trigger.h"
+#include "Sentient.h"
+class Item : public Trigger
+{
+#ifndef MOHAA
+#error "Only Mohaa supported for this class."
+#endif // MOHAA
+    SafePtr<Sentient> owner; /* bitsize 128, bitpos 7968 */
+    qboolean respawnable; /* bitsize 32, bitpos 8096 */
+    qboolean playrespawn; /* bitsize 32, bitpos 8128 */
+    float respawntime; /* bitsize 32, bitpos 8160 */
+    str dialog_needed; /* bitsize 32, bitpos 8192 */
+    int item_index; /* bitsize 32, bitpos 8224 */
+    str item_name; /* bitsize 32, bitpos 8256 */
+    int maximum_amount; /* bitsize 32, bitpos 8288 */
+    int amount; /* bitsize 32, bitpos 8320 */
+    str sPickupSound; /* bitsize 32, bitpos 8352 */
+    qboolean no_remove; /* bitsize 32, bitpos 8384 */
+};
