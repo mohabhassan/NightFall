c6e97fd84c2cb4e43eb40813c3cfa3c88eef14fd
mohabhassan <mohabhassan312@gmail.com>
2020-12-16 16:57:33 +0200

    Implement SafePtr operators

--- src/safeptr.h
+++ src/safeptr.h
@@ -18,5 +18,77 @@ public:
 template<class T>
 class SafePtr : public SafePtrBase
 {
+public:
+	template< class U > friend bool operator==(SafePtr<U> a, U* b);
+	template< class U > friend bool operator!=(SafePtr<U> a, U* b);
+	template< class U > friend bool operator==(U* a, SafePtr<U> b);
+	template< class U > friend bool operator!=(U* a, SafePtr<U> b);
+	template< class U > friend bool operator==(SafePtr<U> a, SafePtr<U> b);
+	template< class U > friend bool operator!=(SafePtr<U> a, SafePtr<U> b);
 
+	bool operator !() const;
+	operator T* () const;
+	T* operator->() const;
+	T& operator*() const;
 };
+
+
+template<class T>
+inline bool operator==(SafePtr<T> a, T* b)
+{
+	return a.ptr == b;
+}
+
+template<class T>
+inline bool operator!=(SafePtr<T> a, T* b)
+{
+	return a.ptr != b;
+}
+
+template<class T>
+inline bool operator==(T* a, SafePtr<T> b)
+{
+	return a == b.ptr;
+}
+
+template<class T>
+inline bool operator!=(T* a, SafePtr<T> b)
+{
+	return a != b.ptr;
+}
+
+template<class T>
+inline bool operator==(SafePtr<T> a, SafePtr<T> b)
+{
+	return a.ptr == b.ptr;
+}
+
+template<class T>
+inline bool operator!=(SafePtr<T> a, SafePtr<T> b)
+{
+	return a.ptr != b.ptr;
+}
+
+template<class T>
+inline bool SafePtr<T>::operator !() const
+{
+	return ptr == NULL;
+}
+
+template<class T>
+inline SafePtr<T>::operator T* () const
+{
+	return (T*)ptr;
+}
+
+template<class T>
+inline T* SafePtr<T>::operator->() const
+{
+	return (T*)ptr;
+}
+
+template<class T>
+inline T& SafePtr<T>::operator*() const
+{
+	return *(T*)ptr;
+}
