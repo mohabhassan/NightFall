4a74d755728b6f658eba3b5469a5a6888901cea5
mohabhassan <mohabhassan312@gmail.com>
2023-02-20 06:35:22 +0200

    Add CrashReporter project and base code

---
+++ CrashReporter/src/main.cpp
@@ -0,0 +1,7 @@
+#include <Windows.h>
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+    /* do your stuff here. If you return from this function the program ends */
+    return 0;
+}
