1b6531803dabbfd2622f42864256b4644dbe6aba
mohabhassan <mohabhassan312@gmail.com>
2023-04-21 21:09:07 +0200

    Upgrade version to v1.1.1

--- dgamexwrapper/src/Version.h
+++ dgamexwrapper/src/Version.h
@@ -1,5 +1,5 @@
 #pragma once
 #define PATCH_NAME "NightFall"
 //don't remove this macro from this file as it's used in building/packaging scripts
-#define PATCH_VERSION "1.1.0"
+#define PATCH_VERSION "1.1.1"
 #define PATCH_STAGE "stable"
